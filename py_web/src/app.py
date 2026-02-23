62c156e2e238ab5e2f38e71b6f20824ed4f919e4
Owor Patrick Ikongha <patrick.ikongha@gmail.com>
2026-02-23 16:27:42 +0100

    added jwt support

--- py_web/src/app.py
+++ py_web/src/app.py
@@ -40,6 +40,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({
+            "message": "error",
             "detail": err_message,
         }),
     )
