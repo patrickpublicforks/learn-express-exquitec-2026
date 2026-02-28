88e97d6948b64f035eb721a601663d80cc8b2b5e
joydee-web <olupayimojoy@gmail.com>
2026-02-28 21:35:44 +0100

    Cleaned repo: removed cache and database files

--- src/models/init.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import uvicorn
-import os
-
-def main():
-    from src.app import app   # import here to avoid circular import
-    uvicorn.run(app, port=int(os.getenv("PORT", 8000)))
